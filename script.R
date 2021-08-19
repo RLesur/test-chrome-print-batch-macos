bff49bbafb0eff37b50e757904c781407ccbb408
Romain Lesur <RLesur@users.noreply.github.com>
2021-08-19 10:38:06 +0200

    Increase to 800 see whether we get an error

--- script.R
+++ script.R
@@ -2,7 +2,7 @@ writeLines('Test', 'test.html')
 
 options(pagedown.chrome.log = TRUE)
 
-for (i in 1:413){
+for (i in 1:800){
   cat('\nAttempt ', i, '\n')
   xfun::Rscript_call('pagedown::chrome_print', list('test.html', wait = 0, verbose = 1))
 }
